1588fa51cdae0ed6d6f0ade9bc290f06ec19a2a1
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2026-05-27 23:54:31 -0500

    Initial commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.mocoding.pokedex.android"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        applicationId = "com.mocoding.pokedex.android"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.appVersionCode.get().toInt()
+        versionName = "${libs.versions.appMajorVersion.get()}.${libs.versions.appMinorVersion.get()}.${libs.versions.appPatchVersion.get()}"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+    packaging {
+        resources {
+            pickFirsts.add(
+                "META-INF/INDEX.LIST"
+            )
+            excludes.addAll(
+                listOf(
+                    "META-INF/AL2.0",
+                    "META-INF/LGPL2.1",
+                ),
+            )
+        }
+    }
+    buildTypes {
+
+        getByName("release") {
+            isMinifyEnabled = true
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(libs.androidx.activity.compose)
+
+    // Koin
+    api(libs.koin.android)
+}
